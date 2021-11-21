02f81d1045ff5dcc3e41982d37463e64a4bebfda
lawrencegoodwyn <89168119+lawrencegoodwyn@users.noreply.github.com>
2021-11-21 14:40:59 -0500

    deliverable 2

--- charts.js
+++ charts.js
@@ -162,10 +162,6 @@ function buildCharts(sample) {
   // Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     
-
-    // Deliverable 1 Step 10. Use Plotly to plot the data with the layout. 
-    Plotly.newPlot(); 
-
     // 1. Create the trace for the bubble chart.
     var bubbleData = [{
     x: ids,
